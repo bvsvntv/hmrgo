b49a067728897f5fe26b6ef937091fb5faaed58b
bvsvntv <developer.basanta@gmail.com>
2026-03-30 15:30:28 +0545

    Serve a static file

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	visitHandler := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "hmrgo: understanding hot module replacement")
-	}
-
-	http.HandleFunc("/", visitHandler)
+	fileServer := http.FileServer(http.Dir("."))
+	http.Handle("/", fileServer)
 
 	log.Println("Serving on http://localhost:2002")
 	if err := http.ListenAndServe(":2002", nil); err != nil {
