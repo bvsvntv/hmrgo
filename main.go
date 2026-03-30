54614db55611868c0d49cbd24dd28f8fab8441ea
bvsvntv <developer.basanta@gmail.com>
2026-03-30 16:17:39 +0545

    Provide value for port as cli flag

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	port := flag.String("port", "2002", "port to listen on")
+	flag.Parse()
+
 	fileServer := http.FileServer(http.Dir("."))
 	http.Handle("/", fileServer)
 
-	log.Println("Serving on http://localhost:2002")
+	fmt.Printf("] Serving on http://localhost:%s\n", *port)
 	if err := http.ListenAndServe(":2002", nil); err != nil {
 		log.Fatal(err)
 	}
