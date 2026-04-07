abd0e5e1f0fdca5bbbddaf3a0d83ddcf68881bc4
bvsvntv <developer.basanta@gmail.com>
2026-04-07 13:56:20 +0545

    Get outbound IP and combine it in network URL

--- main.go
+++ main.go
@@ -24,10 +24,24 @@ func main() {
 
 	fmt.Printf("] Local: http://localhost:%s\n", *port)
 	if *host {
-		fmt.Printf("] Network: <show_local_ip>:%s\n", *port)
+		fmt.Printf("] Network: http://%s:%s\n", getOutboundIP(), *port)
 	}
 
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Get preferred outbound ip of this machine
+// reference: https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
+func getOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
