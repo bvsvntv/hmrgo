5158a8edc8c8b148a6fdba381e9d64d34abdddf4
bvsvntv <developer.basanta@gmail.com>
2026-04-07 13:49:34 +0545

    Expose app on local network

--- main.go
+++ main.go
@@ -10,15 +10,23 @@ import (
 
 func main() {
 	port := flag.String("port", "2002", "port to listen on")
+	host := flag.Bool("host", false, "expose on LAN")
 	flag.Parse()
 
 	fileServer := http.FileServer(http.Dir("."))
 	http.Handle("/", fileServer)
 
 	bind := "127.0.0.1"
+	if *host {
+		bind = "0.0.0.0"
+	}
 	addr := net.JoinHostPort(bind, *port)
 
 	fmt.Printf("] Local: http://localhost:%s\n", *port)
+	if *host {
+		fmt.Printf("] Network: <show_local_ip>:%s\n", *port)
+	}
+
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal(err)
 	}
